69c28082662f26f2755141026f24e26681b9514d
muhsina26 <tasnim15-5763@diu.edu.bd>
2025-03-26 14:37:33 +0600

    task5

---
+++ task5/lib/first_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:task5/second_screen.dart';
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('First Screen')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondScreen()),
+            );
+          },
+          child: Text('Go to Second Screen'),
+        ),
+      ),
+    );
+  }
+}
---
+++ task5/lib/second_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text('Back to First Screen'),
+            ),
+      ),
+    );
+  }
+}
